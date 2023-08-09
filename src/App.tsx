0f5eaf1f01ae2d86369e71f2cf7d4db5752606a8
iDevYT <alphangred57@gmail.com>
2023-08-09 17:55:03 +0200

    💄 changed the logo

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <div className="App">
         <header className='h-[10vh] sticky top-0 dark:bg-zinc-900 bg-zinc-50 flex items-center justify-between px-8'>
-          <img src="/public/nvglogo.png" alt="Novagon Logo" />
+          <img src="https://novagoncdn.netlify.app/logo/novagon/Project%20Novagon%20Logo.svg" alt="Novagon Logo" className='w-16 rounded-xl'/>
           <button data-tooltip-id='downloadbtnfail' data-tooltip-content='This part is still in development!' data-tooltip-place='bottom'>Contact</button>
         </header>
       </div>
@@ -29,8 +29,7 @@ function Home() {
   return (
     <>
       <div className='flex flex-col items-center justify-center p-4 m-2 space-y-6 rounded-md dark:bg-zinc-600 bg-zinc-100 h-80'>
-        <h1 className='mx-2 text-3xl font-albertsans'>Welcome to <span className="font-bold text-violet-500">Novagon</span>.</h1>
-        <q className='flex justify-center p-2 before:content-[">"] font-'>It's is better when its open</q>
+        <h1 className='mx-2 text-3xl font-albertsans before:content-["_>_"]'>It's better when its <span className="font-bold text-violet-500">Open</span>.</h1>
       </div>
       <div className='p-2'>
         <h1 className='text-3xl font-bold font-albertsans'>Apps by Novagon</h1>
